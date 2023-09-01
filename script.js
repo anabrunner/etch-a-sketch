ab1d21dba43a67e9f835597e11d24d4bdec57568
anabrunner <126997850+anabrunner@users.noreply.github.com>
2023-08-31 23:00:55 -0700

    Add function for changing div color when click and drag mouse.

--- script.js
+++ script.js
@@ -4,6 +4,17 @@
 let gridSize = 16;
 makeDivs(gridSize);
 
+// Selects the divs in the Sketch Grid container.
+const divs = document.querySelectorAll("#sketchGrid div");
+divs.forEach((div) => {
+  div.addEventListener("mouseover", fillColor);
+});
+
+// Keeps track of whether the mouse is pressed down or not.
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 // Create a grid of square divs that is n x n.
 // Create the divs.
 function makeDivs(n) {
@@ -17,4 +28,12 @@ function makeDivs(n) {
 // Update the CSS grid.
 function makeGrid(n, sketchGrid) {
   sketchGrid.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+}
+
+// Changes div color for drawing on sketch grid.
+function fillColor(e) {
+  let div = this;
+  if (mouseDown) {
+    div.style.backgroundColor = "black";
+  }
 }
