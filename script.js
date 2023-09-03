08e909cb52af747c87654bc263e76355fe1134f6
anabrunner <126997850+anabrunner@users.noreply.github.com>
2023-09-03 14:42:39 -0700

    Add slider to change grid size.

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 // JavaScript for Etch-a-Sketch
 
-// Determines the grid size.
-let gridSize = 16;
-makeDivs(gridSize);
+const slider = document.getElementById("gridSize");
+const sketchGrid = document.getElementById('sketchGrid');
 
-// Selects the divs in the Sketch Grid container.
-const divs = document.querySelectorAll("#sketchGrid div");
-divs.forEach((div) => {
-  div.addEventListener("mouseover", fillColor);
-});
+const DEFAULT_GRID = 16;
+makeDivs(DEFAULT_GRID);
+
+// Determines the grid size using the slider.
+slider.addEventListener("input", newGrid);
 
 // Keeps track of whether the mouse is pressed down or not.
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+// Clears the grid of the previous divs, then calls function to make new divs.
+function newGrid(e) {
+  sketchGrid.innerHTML = "";
+  let n = this.value;
+  makeDivs(n);
+}
 // Create a grid of square divs that is n x n.
 // Create the divs.
 function makeDivs(n) {
-  const sketchGrid = document.getElementById('sketchGrid');
   for (let i = 1; i <= n ** 2; i++) {
     const newDiv = document.createElement('div');
     sketchGrid.appendChild(newDiv);
@@ -28,6 +32,14 @@ function makeDivs(n) {
 // Update the CSS grid.
 function makeGrid(n, sketchGrid) {
   sketchGrid.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+  selectDivs();
+}
+// Selects the divs in the Sketch Grid container.
+function selectDivs() {
+  const divs = document.querySelectorAll("#sketchGrid div");
+  divs.forEach((div) => {
+    div.addEventListener("mouseover", fillColor);
+  });
 }
 
 // Changes div color for drawing on sketch grid.
